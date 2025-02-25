44c034bde25a6139c071a9ed68d00df1071734bb
dlinox <nearlino20@gmail.com>
2025-02-25 12:33:46 -0500

    first commit

--- app/Modules/Enrollment/Routes/api.php
+++ app/Modules/Enrollment/Routes/api.php
@@ -22,7 +22,6 @@ Route::prefix('api/enrollment')->middleware('auth:sanctum')->group(function () {
     Route::post('store-student-enrollment', [EnrollmentController::class, 'storeStudentEnrollment']);
     //storeGroupEnrollment
     Route::post('store-group-enrollment', [EnrollmentController::class, 'storeGroupEnrollment']);
-
     //enabledGroupsEnrollment
     Route::post('enabled-groups-enrollment', [EnrollmentController::class, 'enabledGroupsEnrollment']);
 });
